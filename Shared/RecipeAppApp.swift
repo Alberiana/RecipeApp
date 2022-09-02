6be60459bf1df53e4d100ff2511260dc9cce174b
Alberiana <Alberiana>
2022-09-03 01:39:39 +0200

    Initial Commit

---
+++ Shared/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  Shared
+//
+//  Created by Alberiana on 3.9.22.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
